1e855fd3221a0e303302f70504c8d5a9cfc5cbff
Jlevbury <125767916+Jlevbury@users.noreply.github.com>
2024-04-04 12:03:04 -0400

    initial source push
    initial push of source files for roulette demo.

---
+++ assets/scripts/start_Button.ts
@@ -0,0 +1,14 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('start_Button')
+export class start_Button extends Component {
+    start() {
+
+    }
+
+    update(deltaTime: number) {
+        
+    }
+}
+
